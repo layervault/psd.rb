82f06fc93a6c8428c4cee67e2381ddf256e2922f
Ryan LeFevre <meltingice8917@gmail.com>
2013-05-17 14:19:35 -0400

    Some changes to Node::Layer and an export test

--- examples/export.rb
+++ examples/export.rb
@@ -1,4 +1,7 @@
 require './lib/psd'
 
 psd = PSD.new('examples/images/example.psd')
+psd.parse!
+
+psd.tree.children.last.children.last.top = 200
 psd.export "./test.psd"
--- lib/psd/nodes/layer.rb
+++ lib/psd/nodes/layer.rb
@@ -1,21 +1,28 @@
 class PSD::Node
   class Layer < PSD::Node
+    PROPERTIES = [:name, :left, :right, :top, :bottom, :height, :width]
+
     def initialize(layer)
       @layer = layer
     end
 
-    [:name, :left, :right, :top, :bottom, :rows, :cols, :height, :width].each do |meth|
+    PROPERTIES.each do |meth|
       define_method meth do
         @layer.send(meth)
+      end
+
+      define_method "#{meth}=" do |val|
+        @layer.send("#{meth}=", val)
       end
     end
 
     def to_hash
-      {
-        name: name,
-        height: height,
-        width: width
-      }
+      hash = {}
+      PROPERTIES.each do |p|
+        hash[p] = self.send(p)
+      end
+
+      return hash
     end
   end
 end
