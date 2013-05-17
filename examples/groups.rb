b12e849525cb091160ab57dc1aa9aa2f8d18cdbd
Kelly Sutton <michael.k.sutton@gmail.com>
2013-05-17 12:06:41 -0400

    removal

--- examples/groups.rb
+++ examples/groups.rb
@@ -1,19 +1,6 @@
 require './lib/psd'
 require 'pp'
 
-module PSDOutput
-  def self.print_folders(folder, prefix = [])
-    folder[:layers].each do |layer|
-      puts prefix.join("") + layer[:name]
-      if layer.is_a?(Hash)
-        prefix.push "-> "
-        self.print_folders(layer, prefix)
-        prefix.pop
-      end
-    end
-  end
-end
-
 psd = PSD.new('examples/images/example.psd')
 psd.parse!
 pp psd.tree.to_hash
