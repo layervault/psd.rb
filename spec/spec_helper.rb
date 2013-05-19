6a7fc9dfb29453c7efb0c5565f2115b27ef9aabe
Kelly Sutton <michael.k.sutton@gmail.com>
2013-05-19 19:09:05 -0400

    run all tests on circle ci

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require './lib/psd'
 
 RSpec.configure do |config|
-  config.filter_run :focus => true
+  unless ENV['CIRCLECI']
+    config.filter_run :focus => true
+  end
   config.treat_symbols_as_metadata_keys_with_true_values = true
 end
