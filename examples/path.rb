cba4a3c6c4fcc0705d38b4c2e65239cbcc68b48e
Ryan LeFevre <meltingice8917@gmail.com>
2013-05-19 15:42:29 -0400

    Slightly better example

--- examples/path.rb
+++ examples/path.rb
@@ -4,4 +4,4 @@ require 'pp'
 psd = PSD.new('examples/images/example.psd')
 psd.parse!
 
-pp psd.tree.children_at_path("Version D/Version E/Version F").first.depth
+pp psd.tree.children_at_path("Version D/Version E/Version F").first.to_hash
