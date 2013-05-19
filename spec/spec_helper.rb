f8401b62ff0ec8ad1cf0fa1e4913cdc937878d90
Kelly Sutton <michael.k.sutton@gmail.com>
2013-05-19 19:10:56 -0400

    run EVERYTHING

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,4 +5,5 @@ RSpec.configure do |config|
     config.filter_run :focus => true
   end
   config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
 end
