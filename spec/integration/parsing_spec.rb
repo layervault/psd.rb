ba67cd1ee5bac79f0e8c358bb5e636cde2df82db
Ryan LeFevre <meltingice8917@gmail.com>
2013-07-10 13:54:02 -0400

    Add header spec

--- spec/integration/parsing_spec.rb
+++ spec/integration/parsing_spec.rb
@@ -7,5 +7,34 @@ describe 'Parsing' do
 
   it "should parse without error" do
     @psd.parse!
+    @psd.parsed?.should be_true
+  end
+
+  describe 'Header' do
+    before(:each) do
+      @psd.parse!
+    end
+
+    it "should contain data" do
+      @psd.header.should_not be_nil
+    end
+
+    it "should be the proper version" do
+      @psd.header.version.should == 1
+    end
+
+    it "should have the proper number of channels" do
+      @psd.header.channels.should == 3
+    end
+
+    it "should parse the proper document dimensions" do
+      @psd.header.width.should == 900
+      @psd.header.height.should == 600
+    end
+
+    it "should correctly parse the color mode" do
+      @psd.header.mode.should == 3
+      @psd.header.mode_name.should == 'RGBColor'
+    end
   end
 end
