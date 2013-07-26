bb6e2ddfca92d2cafb21ef688c206f88928bff15
Ryan LeFevre <meltingice8917@gmail.com>
2013-07-26 11:02:05 -0400

    Documented helpers

--- lib/psd/helpers.rb
+++ lib/psd/helpers.rb
@@ -1,26 +1,33 @@
 class PSD
+  # Various helper methods that make accessing PSD data easier since it's
+  # split up among various sections.
   module Helpers
+    # Width of the entire PSD document, in pixels.
     def width
       header.cols
     end
 
+    # Height of the entire PSD document, in pixels.
     def height
       header.rows
     end
 
+    # All of the layers in this document, including section divider layers.
     def layers
       layer_mask.layers
     end
 
+    # All of the layers, but filters out the section dividers.
     def actual_layers
       layers.delete_if { |l| l.folder? || l.folder_end? }
     end
 
+    # All of the folders in the document.
     def folders
       layers.select { |l| l.folder? }
     end
 
-    # constructs a tree of the current document
+    # Constructs a tree of the current document for easy traversal and data access.
     def tree
       @root ||= PSD::Node::Root.new(self)
     end
