022ab2ff35ce7d3991de06fe208b086a7ad1f831
Ryan LeFevre <meltingice8917@gmail.com>
2013-07-27 16:20:15 -0400

    Fix RGB16 processing

--- lib/psd/image_modes/rgb.rb
+++ lib/psd/image_modes/rgb.rb
@@ -23,7 +23,25 @@ class PSD::Image::Mode
     end
 
     def combine_rgb16_channel
+      (0...@num_pixels).step(2) do |i|
+        pixel = {r: 0, g: 0, b: 0, a: 255}
 
+        PSD::Image::CHANNEL_INFO.each_with_index do |chan, index|
+          next if chan[:id] == -1 && channels != 4
+
+          # Not entirely sure why this works...
+          val = @channel_data[i + (@channel_length * index)]
+
+          case chan[:id]
+          when -1 then pixel[:a] = val
+          when 0 then pixel[:r] = val
+          when 1 then pixel[:g] = val
+          when 2 then pixel[:b] = val
+          end
+        end
+
+        @pixel_data.push *pixel.values
+      end
     end
   end
 end
