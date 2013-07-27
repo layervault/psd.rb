0e8c4484545117ab6bb5ecd43505ea485c0da0c0
Ryan LeFevre <meltingice8917@gmail.com>
2013-07-27 16:42:18 -0400

    Oh right, these

---
+++ lib/psd/image_modes/greyscale.rb
@@ -0,0 +1,25 @@
+class PSD::Image::Mode
+  module Greyscale
+    private
+
+    def combine_greyscale8_channel
+      if channels == 2
+        # We have an alpha channel
+        @num_pixels.times do |i|
+          alpha = @channel_data[i]
+          grey = @channel_data[@channel_length + i]
+
+          @pixel_data.push grey, grey, grey, alpha
+        end
+      else
+        @num_pixels.times do |i|
+          @pixel_data.push *([@channel_data[i]] * 3), 255
+        end
+      end
+    end
+
+    def combine_greyscale16_channel
+
+    end
+  end
+end
---
+++ lib/psd/node.rb
@@ -0,0 +1,44 @@
+# Internal structure to help us build trees of a Photoshop documents.
+# A lot of method names borrowed from the Ruby ancestry gem.
+class PSD
+  class Node
+    include Ancestry
+    include Search
+
+    # Default properties that all nodes contain
+    PROPERTIES = [:name, :left, :right, :top, :bottom, :height, :width]
+
+    attr_accessor :parent, :children
+
+    def initialize(layers=[])
+      @children = []
+      layers.each do |layer|
+        layer.parent = self
+        @children << layer
+      end
+    end
+
+    def hidden?
+      !@layer.visible?
+    end
+
+    def visible?
+      @layer.visible?
+    end
+
+    def to_hash
+      hash = {
+        type: nil,
+        visible: visible?,
+        opacity: @layer.opacity / 255.0,
+        blending_mode: @layer.blending_mode
+      }
+
+      PROPERTIES.each do |p|
+        hash[p] = self.send(p)
+      end
+
+      hash
+    end
+  end
+end
