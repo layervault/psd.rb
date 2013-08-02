9fcb7248d473f6c9fcbe65ca83dbdb9f5d4f676c
Ryan LeFevre <meltingice8917@gmail.com>
2013-08-01 22:04:21 -0400

    Uncomment code from example

--- examples/parse.rb
+++ examples/parse.rb
@@ -8,24 +8,24 @@ results = Benchmark.measure "PSD parsing" do
   psd.parse!
 end
 
-# puts "\nVisible Layers:\n===================="
-# psd.layers.each do |layer|
-#   next if layer.folder? || layer.hidden?
+puts "\nVisible Layers:\n===================="
+psd.layers.each do |layer|
+  next if layer.folder? || layer.hidden?
 
-#   puts "Name: #{layer.name}"
-#   puts "Position: top = #{layer.top}, left = #{layer.left}"
-#   puts "Size: width = #{layer.width}, height = #{layer.height}"
-#   puts "Mask: width = #{layer.mask.width}, height = #{layer.mask.height}"
-#   puts "Reference point: #{layer.ref_x}, #{layer.ref_y}"
+  puts "Name: #{layer.name}"
+  puts "Position: top = #{layer.top}, left = #{layer.left}"
+  puts "Size: width = #{layer.width}, height = #{layer.height}"
+  puts "Mask: width = #{layer.mask.width}, height = #{layer.mask.height}"
+  puts "Reference point: #{layer.ref_x}, #{layer.ref_y}"
 
-#   puts ""
-# end
+  puts ""
+end
 
-# puts "\nPSD Info:\n===================="
-# puts "#{psd.width}x#{psd.height} #{psd.header.mode_name}"
-# puts "#{psd.resources.size} resources parsed"
-# puts "#{psd.layers.size} layers, #{psd.folders.size} folders"
+puts "\nPSD Info:\n===================="
+puts "#{psd.width}x#{psd.height} #{psd.header.mode_name}"
+puts "#{psd.resources.size} resources parsed"
+puts "#{psd.layers.size} layers, #{psd.folders.size} folders"
 
-# puts "\nBenchmark Results (seconds):\n===================="
-# puts " "*7 + Benchmark::CAPTION
-# puts "parse: " + results.to_s
+puts "\nBenchmark Results (seconds):\n===================="
+puts " "*7 + Benchmark::CAPTION
+puts "parse: " + results.to_s
