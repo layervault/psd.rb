e1320eef6a86e380c1fc381c7a8dd58a109a923b
Ryan LeFevre <meltingice8917@gmail.com>
2013-08-25 15:05:48 -0400

    Remove unused code

--- lib/psd/image_modes/greyscale.rb
+++ lib/psd/image_modes/greyscale.rb
@@ -2,22 +2,6 @@ class PSD::Image::Mode
   module Greyscale
     private
 
-    def combine_greyscale8_channel
-      if channels == 2
-        # We have an alpha channel
-        @num_pixels.times do |i|
-          alpha = @channel_data[i]
-          grey = @channel_data[@channel_length + i]
-
-          @pixel_data.push grey, grey, grey, alpha
-        end
-      else
-        @num_pixels.times do |i|
-          @pixel_data.push *([@channel_data[i]] * 3), 255
-        end
-      end
-    end
-
     def combine_greyscale_channel
       if channels == 2
         (0...@num_pixels).step(pixel_step) do |i|
