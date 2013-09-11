4a35495b09de0920e3582c4b5c8728ebbf8b279a
Ryan LeFevre <meltingice8917@gmail.com>
2013-09-10 21:42:42 -0400

    Picky

--- examples/export_layer_images.rb
+++ examples/export_layer_images.rb
@@ -7,9 +7,7 @@ psd = PSD.new(file, parse_layer_images: true)
 psd.parse!
 
 psd.tree.descendant_layers.each do |layer|
-  path = layer.path.split('/')
-  path.pop
-  path = path.join('/')
+  path = layer.path.split('/')[0...-1].join('/')
   FileUtils.mkdir_p("output/#{path}")
   layer.image.save_as_png "output/#{layer.path}.png"
 end
