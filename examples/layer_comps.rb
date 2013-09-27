f8084429b3e66b74634cdf31e28f5dee5dfefcfc
Ryan LeFevre <meltingice8917@gmail.com>
2013-09-27 11:17:21 -0400

    Examples

---
+++ examples/layer_comps.rb
@@ -0,0 +1,9 @@
+require 'pp'
+require './lib/psd'
+
+file = ARGV[0] || 'examples/images/comp-example.psd'
+psd = PSD.new(file)
+psd.parse!
+
+# pp psd.layer_comps
+pp psd.tree.filter_by_comp("Comp 1").to_hash
