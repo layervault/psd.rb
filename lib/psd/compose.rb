8c0d01f6be0e8231cd1f60f151a9f276afc46364
Ryan LeFevre <meltingice8917@gmail.com>
2013-10-31 14:44:22 -0400

    Implement overlay

--- lib/psd/compose.rb
+++ lib/psd/compose.rb
@@ -31,6 +31,32 @@ class PSD
       rgba(new_r, new_g, new_b, new_a)
     end
 
+    def overlay(fg, bg)
+      return fg if opaque?(fg) || fully_transparent?(bg)
+      return bg if fully_transparent?(fg)
+
+      if r(bg) > 128
+        new_r = 0xff - 2 * int8_mult(255 - r(fg), 255 - r(bg))
+      else
+        new_r = 2 * int8_mult(r(fg), r(bg))
+      end
+
+      if g(bg) > 128
+        new_g = 0xff - 2 * int8_mult(255 - g(fg), 255 - g(bg))
+      else
+        new_g = 2 * int8_mult(g(fg), g(bg))
+      end
+
+      if b(bg) > 128
+        new_b = 0xff - 2 * int8_mult(255 - b(fg), 255 - b(bg))
+      else
+        new_b = 2 * int8_mult(b(fg), b(bg))
+      end
+
+      new_a = a(fg) + int8_mult(0xff - a(fg), a(bg))
+      rgba(new_r, new_g, new_b, new_a)
+    end
+
     # If the blend mode is missing, we fall back to normal composition.
     def method_missing(method, *args, &block)
       return ChunkyPNG::Color.send(method, *args) if ChunkyPNG::Color.respond_to?(method)
