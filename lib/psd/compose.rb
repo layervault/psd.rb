e9bd4830739fd10d65248f652419b5121686b928
Ryan LeFevre <meltingice8917@gmail.com>
2013-10-31 14:00:10 -0400

    Implement multiply blend mode

--- lib/psd/compose.rb
+++ lib/psd/compose.rb
@@ -7,8 +7,22 @@ class PSD
       ChunkyPNG::Color.compose(*args)
     end
 
+    def multiply(fg, bg)
+      return fg if opaque?(fg) || fully_transparent?(bg)
+      return bg if fully_transparent?(fg)
+
+      a_com = int8_mult(0xff - a(fg), a(bg))
+      new_r = int8_mult(r(fg), r(bg))
+      new_g = int8_mult(g(fg), g(bg))
+      new_b = int8_mult(b(fg), b(bg))
+      new_a = a(fg) + a_com
+
+      rgba(new_r, new_g, new_b, new_a)
+    end
+
     # If the blend mode is missing, we fall back to normal composition.
     def method_missing(method, *args, &block)
+      return ChunkyPNG::Color.send(method, *args) if ChunkyPNG::Color.respond_to?(method)
       normal(*args)
     end
   end
