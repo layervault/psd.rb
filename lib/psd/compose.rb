6bb7b7db9e412a7ad0b88ac76c906d1bc2b8e084
Ryan LeFevre <meltingice8917@gmail.com>
2013-10-31 14:23:37 -0400

    Implement screen blend mode

--- lib/psd/compose.rb
+++ lib/psd/compose.rb
@@ -11,11 +11,22 @@ class PSD
       return fg if opaque?(fg) || fully_transparent?(bg)
       return bg if fully_transparent?(fg)
 
-      a_com = int8_mult(0xff - a(fg), a(bg))
       new_r = int8_mult(r(fg), r(bg))
       new_g = int8_mult(g(fg), g(bg))
       new_b = int8_mult(b(fg), b(bg))
-      new_a = a(fg) + a_com
+      new_a = a(fg) + int8_mult(0xff - a(fg), a(bg))
+
+      rgba(new_r, new_g, new_b, new_a)
+    end
+
+    def screen(fg, bg)
+      return fg if opaque?(fg) || fully_transparent?(bg)
+      return bg if fully_transparent?(fg)
+
+      new_r = 0xff - int8_mult(0xff - r(fg), 0xff - r(bg))
+      new_g = 0xff - int8_mult(0xff - g(fg), 0xff - g(bg))
+      new_b = 0xff - int8_mult(0xff - b(fg), 0xff - b(bg))
+      new_a = a(fg) + int8_mult(0xff - a(fg), a(bg))
 
       rgba(new_r, new_g, new_b, new_a)
     end
