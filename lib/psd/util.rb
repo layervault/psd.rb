8e205744924221445211a88fd2dd3002c953c4ef
Ryan LeFevre <meltingice8917@gmail.com>
2013-11-24 20:16:01 -0500

    Change Util to a module

--- lib/psd/util.rb
+++ lib/psd/util.rb
@@ -1,16 +1,18 @@
 class PSD
-  class Util
+  module Util
+    extend self
+
     # Ensures value is a multiple of 2
-    def self.pad2(i)
+    def pad2(i)
       ((i + 1) / 2) * 2
     end
 
     # Ensures value is a multiple of 4
-    def self.pad4(i)
+    def pad4(i)
       i - (i.modulo(4)) + 3
     end
 
-    def self.clamp(num, min, max)
+    def clamp(num, min, max)
       [min, num, max].sort[1]
     end
   end
