4ae804064c346f9e8540c602f40d7aa3a0978e25
Ryan LeFevre <meltingice8917@gmail.com>
2013-12-05 21:25:38 -0500

    Actually fix merge properly this time

--- lib/psd/image_exports/layer_png.rb
+++ lib/psd/image_exports/layer_png.rb
@@ -5,7 +5,7 @@ class PSD::Image
       # data.
       def to_png(layer_styles=true)
         return @png if @png
-
+        
         PSD.logger.debug "Beginning layer PNG export"
         @png = ChunkyPNG::Canvas.new(width.to_i, height.to_i, ChunkyPNG::Color::TRANSPARENT)
 
@@ -23,17 +23,17 @@ class PSD::Image
       end
       alias :export :to_png
 
-      def to_png_with_mask
-        return to_png unless has_mask?
+      def to_png_with_mask(layer_styles=true)
+        return to_png(layer_styles) unless has_mask?
         return @png_with_mask if @png_with_mask
 
         PSD.logger.debug "Beginning PNG export with mask"
-
+        
         # We generate the preview at the document size instead to make applying the mask
         # significantly easier.
         width = @layer.header.width.to_i
         height = @layer.header.height.to_i
-        @png_with_mask = ChunkyPNG::Canvas.new(width, height, ChunkyPNG::Color::TRANSPARENT)
+        png = ChunkyPNG::Canvas.new(width, height, ChunkyPNG::Color::TRANSPARENT)
 
         i = 0
         @layer.height.times do |y|
@@ -41,13 +41,13 @@ class PSD::Image
             offset_x = x + @layer.left
             offset_y = y + @layer.top
 
-            i +=1 and next if offset_x < 0 || offset_y < 0 || offset_x >= @png_with_mask.width || offset_y >= @png_with_mask.height
+            i +=1 and next if offset_x < 0 || offset_y < 0 || offset_x >= png.width || offset_y >= png.height
 
-            @png_with_mask[offset_x, offset_y] = @pixel_data[i]
+            png[offset_x, offset_y] = @pixel_data[i]
             i += 1
           end
         end
-
+        
         # Now we apply the mask
         i = 0
         @layer.mask.height.times do |y|
@@ -55,22 +55,25 @@ class PSD::Image
             offset_x = @layer.mask.left + x
             offset_y = @layer.mask.top + y
 
-            i += 1 and next if offset_x < 0 || offset_y < 0 || offset_x >= @png_with_mask.width || offset_y >= @png_with_mask.height
+            i += 1 and next if offset_x < 0 || offset_y < 0 || offset_x >= png.width || offset_y >= png.height
 
-            color = ChunkyPNG::Color.to_truecolor_alpha_bytes(@png_with_mask[offset_x, offset_y])
+            color = ChunkyPNG::Color.to_truecolor_alpha_bytes(png[offset_x, offset_y])
             color[3] = color[3] * @mask_data[i] / 255
 
-            @png_with_mask[offset_x, offset_y] = ChunkyPNG::Color.rgba(*color)
+            png[offset_x, offset_y] = ChunkyPNG::Color.rgba(*color)
             i += 1
           end
         end
 
-        crop_left = PSD::Util.clamp(@layer.left, 0, @png_with_mask.width)
-        crop_top = PSD::Util.clamp(@layer.top, 0, @png_with_mask.height)
-        crop_width = PSD::Util.clamp(@layer.width.to_i, 0, @png_with_mask.width - crop_left)
-        crop_height = PSD::Util.clamp(@layer.height.to_i, 0, @png_with_mask.height - crop_top)
+        crop_left = PSD::Util.clamp(@layer.left, 0, png.width)
+        crop_top = PSD::Util.clamp(@layer.top, 0, png.height)
+        crop_width = PSD::Util.clamp(@layer.width.to_i, 0, png.width - crop_left)
+        crop_height = PSD::Util.clamp(@layer.height.to_i, 0, png.height - crop_top)
 
-        @png_with_mask.crop!(crop_left, crop_top, crop_width, crop_height)
+        png.crop!(crop_left, crop_top, crop_width, crop_height)
+        PSD::LayerStyles.new(@layer, transparent_base, png).apply! if layer_styles
+
+        @png_with_mask = png and return @png_with_mask
       end
 
       def mask_to_png
