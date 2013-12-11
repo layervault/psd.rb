069a5215b8344ae73b26c41dbca7ba4120d30d2a
Kelly Sutton <michael.k.sutton@gmail.com>
2013-12-10 22:49:16 -0500

    Make this business a little more rspec-y

--- spec/image_spec.rb
+++ spec/image_spec.rb
@@ -1,39 +1,33 @@
 require 'spec_helper'
 
 describe 'Image Exporting' do
-  before(:each) do
-    class PSD::Image
-      attr_accessor :pixel_data
-    end
-
-    @psd = PSD.new('spec/files/pixel.psd')
-  end
+  let(:psd) { PSD.new('spec/files/pixel.psd') }
 
   describe "the full preview image" do
     it "should successfully parse the image data" do
-      @psd.parse!
-      expect(@psd).to be_parsed
-      expect(@psd.image).to_not be_nil
-      expect(@psd.image.width).to eq(1)
-      expect(@psd.image.height).to eq(1)
-      expect(@psd.image.pixel_data).to eq([ChunkyPNG::Color.rgba(0, 100, 200, 255)])
+      psd.parse!
+      expect(psd).to be_parsed
+      expect(psd.image).to_not be_nil
+      expect(psd.image.width).to eq(1)
+      expect(psd.image.height).to eq(1)
+      expect(psd.image.pixel_data).to eq([ChunkyPNG::Color.rgba(0, 100, 200, 255)])
     end
 
     it "should be able to skip to the image" do
-      expect(@psd).to_not be_parsed
-      expect(@psd.image.width).to eq(1)
-      expect(@psd.image.height).to eq(1)
-      expect(@psd.image.pixel_data).to eq([ChunkyPNG::Color.rgba(0, 100, 200, 255)])
+      expect(psd).to_not be_parsed
+      expect(psd.image.width).to eq(1)
+      expect(psd.image.height).to eq(1)
+      expect(psd.image.pixel_data).to eq([ChunkyPNG::Color.rgba(0, 100, 200, 255)])
     end
 
     describe "as PNG" do
       it "should produce a valid PNG object" do
-        expect(@psd.image.to_png).to be_an_instance_of(ChunkyPNG::Canvas)
+        expect(psd.image.to_png).to be_an_instance_of(ChunkyPNG::Canvas)
 
-        expect(@psd.image.to_png.width).to eq(1)
-        expect(@psd.image.to_png.height).to eq(1)
+        expect(psd.image.to_png.width).to eq(1)
+        expect(psd.image.to_png.height).to eq(1)
         expect(
-          ChunkyPNG::Color.to_truecolor_alpha_bytes(@psd.image.to_png[0,0])
+          ChunkyPNG::Color.to_truecolor_alpha_bytes(psd.image.to_png[0,0])
         ).to eq([0, 100, 200, 255])
       end
     end
@@ -41,10 +35,10 @@ describe 'Image Exporting' do
 
   describe "layer images" do
     it "should successfully parse the image data" do
-      @psd.options[:parse_layer_images] = true
-      @psd.parse!
+      psd.options[:parse_layer_images] = true
+      psd.parse!
 
-      image = @psd.tree.children.first.image
+      image = psd.tree.children.first.image
       expect(image.width).to eq(1)
       expect(image.height).to eq(1)
 
@@ -53,10 +47,10 @@ describe 'Image Exporting' do
 
     describe "as PNG" do
       it "should produce a valid PNG object" do
-        @psd.options[:parse_layer_images] = true
-        @psd.parse!
+        psd.options[:parse_layer_images] = true
+        psd.parse!
 
-        png = @psd.tree.children.first.image.to_png
+        png = psd.tree.children.first.image.to_png
         expect(png).to be_an_instance_of(ChunkyPNG::Canvas)
         expect(png.width).to eq(1)
         expect(png.height).to eq(1)
@@ -66,10 +60,10 @@ describe 'Image Exporting' do
       end
 
       it "memorizes the png instance" do
-        @psd.options[:parse_layer_images] = true
-        @psd.parse!
+        psd.options[:parse_layer_images] = true
+        psd.parse!
 
-        node = @psd.tree.children.first
+        node = psd.tree.children.first
         png  = node.image.to_png
 
         expect(png).to be_an_instance_of(ChunkyPNG::Canvas)
@@ -77,10 +71,10 @@ describe 'Image Exporting' do
       end
 
       it "memorizes the png_with_mask instance" do
-        @psd = PSD.new('spec/files/path.psd', parse_layer_images: true)
-        @psd.parse!
+        psd = PSD.new('spec/files/path.psd', parse_layer_images: true)
+        psd.parse!
 
-        node = @psd.tree.children.first
+        node = psd.tree.children.first
         png  = node.image.to_png_with_mask
 
         expect(png).to be_an_instance_of(ChunkyPNG::Canvas)
