ccf7d5991f4c212d901ebbc59c641545be45b459
Ryan LeFevre <meltingice8917@gmail.com>
2014-04-18 09:08:33 -0400

    Add XMP resource

---
+++ lib/psd/resources/xmp_metadata.rb
@@ -0,0 +1,45 @@
+require 'psd/resources/base'
+
+class PSD
+  class Resource
+    module Section
+      class XMPMetadata < Base
+        resource_id 1060
+        name :xmp_metadata
+
+        attr_reader :xml, :data
+        alias_method :to_hash, :data
+
+        def parse
+          @xml = @file.read(@resource.size)
+          @xmp = XMP.new(xml)
+
+          @data = {}
+
+          @xmp.namespaces.each do |a|
+            parse_tree(a.to_sym)
+          end
+
+          @resource.data = self
+        end
+        
+        private
+
+        def parse_tree(attr_name)
+          @data[attr_name] = {}
+          @xmp.send(attr_name).attributes.each do |a|
+            begin
+              @data[attr_name][a.to_sym] = @xmp.send(attr_name).send(a)[0]
+            rescue
+              @data[attr_name][a.to_sym] = parse_text_node(@xmp.send(attr_name), "//#{attr_name}:#{a}")
+            end
+          end
+        end
+
+        def parse_text_node(node, path)
+          node.send(:xml).xpath(path).first.text
+        end
+      end
+    end
+  end
+end
