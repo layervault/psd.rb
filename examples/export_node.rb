ca76401f7b656bce5d31dcc300916fbd90450646
Ryan LeFevre <meltingice8917@gmail.com>
2014-05-20 08:46:02 -0400

    Remove old example file

--- examples/export_node.rb
+++
@@ -1,7 +0,0 @@
-require './lib/psd'
-
-psd = PSD.new('/Users/ryanlefevre/Downloads/c7d9151aa6bc8511124b161e862f87c1.psd')
-psd.parse!
-
-
-psd.tree.children_at_path(["LIST VIEW screen", "Tabbar", "Tab Bar", "icons", "mysunbelt-icon@2x.png"]).first.save_as_png("./output.png")
